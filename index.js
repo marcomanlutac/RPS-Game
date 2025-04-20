93ce86f6d536d60532a69a11a426073217ce8b3e
marcomanlutac <marcomanlutac12@gmail.com>
2025-04-20 11:05:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
+let pscore = 0
+let cscore = 0
+
 function play(playerChoice){
     const choices = ['rock', 'paper', 'scissor']
     let computerChoice = choices[Math.floor(Math.random() * 3)];
-
     let result = ''
-    let choices_print = ''
+    let player_choices_print = ''
+    let comp_choice_print = ''
 
     if (playerChoice === computerChoice){
         result = `It's a tie!`
-        choices_print = `Both choose ${playerChoice}`
+        player_choices_print = `Player picked: ${playerChoice}`
+        comp_choice_print = `Computer picked: ${computerChoice}`
     } else if (
         (playerChoice === 'rock' && computerChoice === 'scissor') ||
         (playerChoice === 'paper' && computerChoice == 'rock') ||
         (playerChoice === 'scissor' && computerChoice == 'paper')
     ) {
-        result = `You won!`
-        choices_print = `${playerChoice} beats ${computerChoice}`
+        result = `Player Won!`
+        player_choices_print = `Player picked: ${playerChoice}`
+        comp_choice_print = `Computer picked: ${computerChoice}`
+        pscore += 1
     } else{
-        result = `You Lost!`
-        choices_print = `${computerChoice} beats ${playerChoice}`
+        result = `Computer Won!`
+        player_choices_print = `Player picked: ${playerChoice}`
+        comp_choice_print = `Computer picked: ${computerChoice}`
+        cscore += 1
     }
-
         document.getElementById("result").innerHTML = result
-        document.getElementById("prompt").innerHTML = choices_print
+        document.getElementById("choice1").innerHTML = player_choices_print
+        document.getElementById("choice2").innerHTML = comp_choice_print
+        document.getElementById("pscore").innerHTML = pscore
+        document.getElementById("cscore").innerHTML = cscore
 
     }
